b5290eb42a553696e924b0df6876f94817889a20
Christian Fürst <c.fuerst.home@gmail.com>
2024-01-12 10:31:07 +0100

    added search filter

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,25 +1,57 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 import { BookCardComponent } from './book-card/book-card.component';
+import { BookFilterPipe } from './book-filter.pipe';
 import { Book } from '../types/book';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, MatToolbarModule, BookCardComponent],
+  imports: [
+    CommonModule,
+    MatToolbarModule,
+    MatGridListModule,
+    MatInputModule,
+    MatFormFieldModule,
+    BookCardComponent,
+    BookFilterPipe,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'Hello World!';
+  bookFilter = '';
 
-  book = {
-    title: 'How to win friends',
-    author: 'Dale Carnegie',
-    abstract: 'In this book ...',
-  };
+  books: Book[] = [
+    {
+      title: 'How to win friends',
+      author: 'Dale Carnegie',
+      abstract: 'How to Win Friends and Influence ...',
+    },
+    {
+      title: 'The Willpower Instinct: How Self-Control Works ...',
+      author: 'Kelly McGonigal',
+      abstract: 'Based on Stanford University ...',
+    },
+    {
+      author: 'Simon Sinek',
+      title: 'Start with WHY',
+      abstract: "START WITH WHY shows that the leaders who've ...",
+    },
+  ];
+
+  filterbooks(book: Book): Book[] {
+    return this.books.filter((b) => b.title.includes(book.title));
+  }
+
+  updateBookFilter(input: Event) {
+    this.bookFilter = (input.target as HTMLInputElement).value;
+  }
 
   navigate(book: Book) {
     console.log('Navigate to book details, soon...');
---
+++ src/app/book-filter.pipe.ts
@@ -0,0 +1,22 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+import { Book } from '../types/book';
+
+@Pipe({
+  name: 'bookFilter',
+  standalone: true,
+})
+export class BookFilterPipe implements PipeTransform {
+  transform(books: Book[], searchTerm: string): Book[] {
+    if (!searchTerm) {
+      return books;
+    }
+
+    //Ignore case
+    searchTerm = searchTerm.toLocaleLowerCase();
+
+    return books.filter((b) =>
+      b.title.toLocaleLowerCase().includes(searchTerm)
+    );
+  }
+}
