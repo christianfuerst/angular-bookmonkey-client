2f7445ef8fb02099b8faaca6c65fbed1384dfe26
Christian Fürst <c.fuerst.home@gmail.com>
2024-01-12 11:50:22 +0100

    refactoring

--- src/app/book/book-card/book-card.component.ts
+++ src/app/book/book-card/book-card.component.ts
@@ -2,7 +2,7 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 
-import { Book } from '../../types/book';
+import { Book } from '../types/book';
 
 @Component({
   selector: 'app-book-card',
--- src/app/book/book-filter.pipe.ts
+++ src/app/book/book-filter.pipe.ts
@@ -1,6 +1,6 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
-import { Book } from '../types/book';
+import { Book } from './types/book';
 
 @Pipe({
   name: 'bookFilter',
