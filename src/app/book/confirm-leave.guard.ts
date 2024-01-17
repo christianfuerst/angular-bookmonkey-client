ebb151870669a23cd1775abdbcc25967af697cfd
Christian Fürst <c.fuerst.home@gmail.com>
2024-01-17 12:27:43 +0100

    refactoring

--- src/app/book/confirm-leave.guard.ts
+++ src/app/book/confirm-leave.guard.ts
@@ -1,5 +1,15 @@
-import { CanDeactivateFn } from '@angular/router';
+import {
+  ActivatedRouteSnapshot,
+  CanDeactivateFn,
+  RouterStateSnapshot,
+} from '@angular/router';
+import { BookDetailComponent } from './book-detail/book-detail.component';
 
-export const confirmLeaveGuard: CanDeactivateFn<unknown> = () => {
-  return confirm('Do you really want to leave?');
+export const confirmLeaveGuard: CanDeactivateFn<BookDetailComponent> = (
+  component: BookDetailComponent,
+  currentRoute: ActivatedRouteSnapshot,
+  currentState: RouterStateSnapshot,
+  nextState: RouterStateSnapshot
+) => {
+  return window.confirm('Do you really want to leave?');
 };
