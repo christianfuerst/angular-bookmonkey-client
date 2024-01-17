6f87cae6ae343da2447eb0ef3f2945938808fe34
Christian Fürst <c.fuerst.home@gmail.com>
2024-01-17 09:14:48 +0100

    refactoring

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,12 @@ import { Component } from '@angular/core';
 import { BookComponent } from './book/book.component';
 import { RouterOutlet } from '@angular/router';
 
+import { NavigationComponent } from './navigation/navigation.component';
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [BookComponent, RouterOutlet],
+  imports: [NavigationComponent, BookComponent, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
